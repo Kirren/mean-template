592269d8a182cd0cc3cf78e335e46a1a556d67c7
Helen <kirren@live.com>
2020-03-25 19:42:04 +0200

    Import necessary modules

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,16 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from '@components/post-create/post-create.component';
+import { FormsModule } from '@base/node_modules/@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@base/node_modules/@angular/material/form-field';
+import { MatButtonModule } from '@base/node_modules/@angular/material/button';
+import { MatInputModule } from '@base/node_modules/@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent
     AppComponent,
     PostCreateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
