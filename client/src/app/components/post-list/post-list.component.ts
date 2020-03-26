71cfbcec21c3693f60d18fe37b587b35f058c1b6
Helen <kirren@live.com>
2020-03-26 19:43:16 +0200

    Move posts to parent component

--- client/src/app/components/post-list/post-list.component.ts
+++ client/src/app/components/post-list/post-list.component.ts
@@ -1,65 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-
-interface IPost {
-  id: number,
-  title: string,
-  content: string
-}
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.scss']
 })
-export class PostListComponent implements OnInit {
-  posts: IPost[] = [
-    {
-      'id': 1,
-      'title': 'Grass-roots contextually-based workforce',
-      'content': 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.\n\nInteger tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.'
-    }, {
-      'id': 2,
-      'title': 'Managed multi-state monitoring',
-      'content': 'Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.\n\nPhasellus in felis. Donec semper sapien a libero. Nam dui.'
-    }, {
-      'id': 3,
-      'title': 'Integrated zero administration function',
-      'content': 'Fusce consequat. Nulla nisl. Nunc nisl.\n\nDuis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.\n\nIn hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.'
-    }, {
-      'id': 4,
-      'title': 'Profound cohesive Graphic Interface',
-      'content': 'Fusce consequat. Nulla nisl. Nunc nisl.\n\nDuis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.'
-    }, {
-      'id': 5,
-      'title': 'Future-proofed exuding open architecture',
-      'content': 'In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.\n\nNulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.'
-    }, {
-      'id': 6,
-      'title': 'Reduced contextually-based knowledge user',
-      'content': 'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.\n\nPraesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.'
-    }, {
-      'id': 7,
-      'title': 'Inverse 24/7 Graphical User Interface',
-      'content': 'Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.\n\nDuis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.\n\nMauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.'
-    }, {
-      'id': 8,
-      'title': 'Optimized encompassing data-warehouse',
-      'content': 'Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.\n\nPhasellus in felis. Donec semper sapien a libero. Nam dui.\n\nProin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.'
-    }, {
-      'id': 9,
-      'title': 'Cloned dynamic knowledge user',
-      'content': 'Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.\n\nMorbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.'
-    }, {
-      'id': 10,
-      'title': 'Quality-focused optimal hardware',
-      'content': 'Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.\n\nNullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.\n\nMorbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.'
-    }
-  ];
-
-  constructor() {
-  }
-
-  ngOnInit(): void {
-  }
-
+export class PostListComponent {
+  @Input("posts") posts;
 }
