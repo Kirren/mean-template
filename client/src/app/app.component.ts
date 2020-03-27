768137f18a9ed91a08f592b055c062ce62eeaad1
Helen <kirren@live.com>
2020-03-27 17:04:46 +0200

    Fix some styling/linting errors

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Post } from '@app/interfaces/post';
+import { IPost } from '@app/interfaces/IPost';
 
 @Component({
 	selector: 'app-root',
@@ -9,23 +9,34 @@ import { Post } from '@app/interfaces/post';
 export class AppComponent {
 	public title = 'client';
 
-	public posts: Post[] = [
+	public posts: IPost[] = [
 		{
 			title: 'Grass-roots contextually-based workforce',
-			content: 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.\n\nInteger tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.'
+			content: 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst.' +
+				' Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, ' +
+				'lorem.\n\nInteger tincidunt ante vel ipsum. Praesent blandit lacinia erat.' +
+				' Vestibulum sed magna at nunc commodo placerat.'
 		}, {
 			title: 'Managed multi-state monitoring',
-			content: 'Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.\n\nPhasellus in felis. Donec semper sapien a libero. Nam dui.'
+			content: 'Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue' +
+				' eget, semper rutrum, nulla. Nunc purus.\n\nPhasellus in felis. Donec semper' +
+				' sapien a libero. Nam dui.'
 		}, {
 			title: 'Integrated zero administration function',
-			content: 'Fusce consequat. Nulla nisl. Nunc nisl.\n\nDuis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.\n\nIn hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.'
+			content: 'Fusce consequat. Nulla nisl. Nunc nisl.\n\nDuis bibendum, felis sed ' +
+				'interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.' +
+				' Donec dapibus. Duis at velit eu est congue elementum.\n\nIn hac habitasse ' +
+				'platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum' +
+				' justo, nec condimentum neque sapien placerat ante. Nulla justo.'
 		}, {
 			title: 'Profound cohesive Graphic Interface',
-			content: 'Fusce consequat. Nulla nisl. Nunc nisl.\n\nDuis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.'
+			content: 'Fusce consequat. Nulla nisl. Nunc nisl.\n\nDuis bibendum, felis sed ' +
+				'interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.' +
+				' Donec dapibus. Duis at velit eu est congue elementum.'
 		}
 	];
 
-	public addNewPost(post) {
+	public addNewPost(post: IPost): void {
 		this.posts.push(post);
 	}
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,17 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from '@components/post-create/post-create.component';
-import { FormsModule } from '@base/node_modules/@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatFormFieldModule } from '@base/node_modules/@angular/material/form-field';
-import { MatButtonModule } from '@base/node_modules/@angular/material/button';
-import { MatInputModule } from '@base/node_modules/@angular/material/input';
-import { MatCardModule } from '@base/node_modules/@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
+
 import { HeaderComponent } from '@components/header/header.component';
-import { MatToolbarModule } from '@base/node_modules/@angular/material/toolbar';
-import { PostListComponent } from './components/post-list/post-list.component';
-import { MatExpansionModule } from '@base/node_modules/@angular/material/expansion';
+import { PostListComponent } from '@components/post-list/post-list.component';
 
 @NgModule({
 	declarations: [
@@ -33,9 +34,10 @@ import { MatExpansionModule } from '@base/node_modules/@angular/material/expansi
 		MatInputModule,
 		MatCardModule,
 		MatToolbarModule,
-		MatExpansionModule
+		MatExpansionModule,
+		ReactiveFormsModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
